26219a22877aad78457ad0d3a9bff9e9bdee7135
Jeff Bass <jeff@yin-yang-ranch.com>
2022-01-11 12:51:05 -0800

    Update __version__.py

--- wifi-watcher/__version__.py
+++ wifi-watcher/__version__.py
@@ -2,7 +2,7 @@
 __title__ = 'wifi-watcher'
 __description__ = 'Watch for WiFi signal. Restart Wifi router if absent'
 __url__ = 'https://github.com/jeffbass/wifi-watcher'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Jeff Bass'
 __author_email__ = 'jeff@yin-yan-ranch.com'
 __license__ = 'MIT'
